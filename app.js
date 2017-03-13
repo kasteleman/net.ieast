391813b7c6a6182e9c4d3844c8061e87ae38d3c9
kasteleman <kasteleman@gmail.com>
2017-03-13 23:35:47 +0100

    basic functions

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const Client = require('node-ssdp').Client;
 const client = new Client();
 const playersfound = [];
 const playerxml = ':49152/description.xml';
+const httpapi = '/httpapi.asp?command=';
+const playerstatus = 'getPlayerStatus';
+const setplayer = 'setPlayerCmd:';
 const request = require('request');
 // list of currently configured devices
 const devices = {};
-// list of currently found devices
-let clients = {};
 
 const manufacturers = {
 	iEAST: {
 		hardware: 'A11',
+		icon: 'audiocast.svg',
 	},
 	'Linkplay Technology Inc.': {
 		hardware: 'A31',
+		icon: 'audiocast.svg',
 	},
 };
 
@@ -23,7 +26,8 @@ const manufacturers = {
 function getPlayersaddresses() {
 	let Playeraddress;
 	let Playerheaders = [];
-	client.on('response', function inResponse(headers, code, rinfo) {
+	client.on('response', (headers, code, rinfo) => {
+	// client.on('response', function inResponse(headers, code, rinfo) {
 		// console.log(JSON.parse(JSON.stringify(rinfo, null, '  ')));
 		// console.log(JSON.parse(JSON.stringify(headers, null, '  ')));
 		Playeraddress = JSON.parse(JSON.stringify(rinfo, null, '  '));
@@ -39,72 +43,101 @@ function getPlayersaddresses() {
 			}
 		}
 		console.log(playersfound);
+		return playersfound;
 	});
 
 	// client.search('urn:schemas-tencent-com:service:QPlay:1');
 	client.search('ssdp:all');
 	// And after 10 seconds, you want to stop
-	setTimeout(function () {
+	setTimeout(() => {
 		client.stop();
 	}, 10000);
 }
 
+// get data from description.xml
+
 function checkdescriptionxml(ip) {
-	const url = 'http://' + ip + playerxml;
+	const url = `http://${ip}${playerxml}`;
 	const options = {
 		method: 'GET',
 		uri: url,
 	};
-	request(url, function (error, response, body) {
+	request(options, (error, response, body) => {
 		// Check for error
 		if (error) {
 			return console.log('Error: ', error);
 		}
 		if (!error && response.statusCode === 200) {
-			const result = [];
 			// console.log(JSON.stringify(body, null, ''));
 			// const manufacturer = body.substring(body.indexOf('<manufacturer>') + 14, body.indexOf('</manufacturer>'));
 			// console.log(manufacturer);
 			// const friendlyName = body.substring(body.indexOf('<friendlyName>') + 14, body.indexOf('</friendlyName>'));
 			// console.log(friendlyName)
 			// if ()
-			result.push({
-				devicemanufacturer: body.substring(body.indexOf('<manufacturer>') + 14, body.indexOf('</manufacturer>')),
-				devicefriendlyname: body.substring(body.indexOf('<friendlyName>') + 14, body.indexOf('</friendlyName>')),
-				deviceaddress: ip,
-			});
+			const result = {
+				// Set devicename the friendlyName
+				name: body.substring(body.indexOf('<friendlyName>') + 14, body.indexOf('</friendlyName>')),
+				data: {
+					manufacturer: body.substring(body.indexOf('<manufacturer>') + 14, body.indexOf('</manufacturer>')),
+					address: ip,
+				},
+			};
 			console.log(result);
 			return result;
 		}
 	});
 }
 
-module.exports.init = () => {
+// Get the player status
+
+function getplayerstatus(ip, devicename) {
+	const url = `http://${ip}${httpapi}${playerstatus}`;
+	const options = {
+		method: 'GET',
+		uri: url,
+	};
+	console.log(url);
+	request(options, (error, response, body) => {
+		// Check for error
+		if (error) {
+			return console.log('Error: ', error);
+		}
+		if (!error && response.statusCode === 200) {
+			const status = console.log(JSON.parse(body, null, ''));
+			console.log(status);
+			// return status;
+		}
+	});
+}
+
+// Send command to the player
+
+function setplayercommand(ip, setcommand) {
+	const url = `http://${ip}${httpapi}${setplayer}`;
+	const options = {
+		method: 'POST',
+		uri: url,
+	};
+	console.log(url);
+	request(options, (error, response, body) => {
+		// Check for error
+		if (error) {
+			return console.log('Error: ', error);
+		}
+		if (!error && response.statusCode === 200) {
+			const feedback = console.log(JSON.parse(body, null, ''));
+			console.log(feedback);
+			// return feedback;
+		}
+	});
+}
+
+module.exports.init = (devicesData, callback) => {
 	console.log('iEast AudioCast app starting');
 	// Get the possible playerdevices ip addresses
 	getPlayersaddresses();
-	playersfound.push('192.168.2.23');
-	// const output = playersfound.forEach(checkdescriptionxml);
-	// console.log(output);
-};
-
-/*
- * This method will be run when starting to pair.
- */
-module.exports.pair = (socket) => {
-	socket.on('list_devices', (data, callback) => {
-//			getJson().then(json => {
-//				const result = parseJson(json);
-//				const listDevices = getListDevices(result);
-		console.log('searching for devices');
-		const listDevices = playersfound.forEach(checkdescriptionxml);
-		callback(null, listDevices);
-//			}).catch(function(error) {
-//				_debug('Failed to get a list of devices', error);
-//				callback('Failed to get a list of devices.', null);
-//			});
-	});
-	socket.on('disconnect', () => {
-		console.log('User aborted pairing, or pairing is finished');
-	});
+	// playersfound.push('192.168.2.23');
+	const output = playersfound.forEach(checkdescriptionxml);
+	console.log(output);
+	getplayerstatus('192.168.2.22', 'Living Room');
 };
--- drivers/iEast/drivers.js
+++ drivers/iEast/drivers.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const Client = require('node-ssdp').Client;
-const client = new Client();
-const playersfound = [];
-
-// Get IP addresses of potential iEast players
-function getPlayersaddresses() {
-	let Playeraddress;
-	client.on('response', function inResponse(headers, code, rinfo) {
-		Homey.log(JSON.stringify(headers, null, '  '));
-		Homey.log(JSON.stringify(rinfo, null, '  '));
-		Playeraddress = JSON.parse(JSON.stringify(rinfo, null, '  '));
-		if (playersfound.indexOf(Playeraddress.address) === -1 || playersfound.length === 0) {
-			playersfound.push(Playeraddress.address);
-		}
-		Homey.log(playersfound);
-	});
-
-	// client.search('urn:schemas-tencent-com:service:QPlay:1');
-	client.search('ssdp:all');
-}
+module.exports.init = (devicesData, callback) => {
+	console.log('iEast AudioCast app starting');
+};
